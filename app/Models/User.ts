6fac616ccff0838908884e62cafd6c9e8c7e03d9
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2022-04-16 17:01:01 +0800

    add: UserKeys model created

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -1,10 +1,17 @@
 import { DateTime } from 'luxon'
 import Hash from '@ioc:Adonis/Core/Hash'
-import { column, beforeSave, BaseModel } from '@ioc:Adonis/Lucid/Orm'
+import { column, beforeSave, BaseModel, HasMany, hasMany } from '@ioc:Adonis/Lucid/Orm'
+import { UserKey } from 'App/Models'
 
 export default class User extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column()
+  public name: string
+
+  @column()
+  public username: string
 
   @column()
   public email: string
@@ -15,6 +22,9 @@ export default class User extends BaseModel {
   @column()
   public rememberMeToken?: string
 
+  @hasMany(() => UserKey)
+  public userKeys: HasMany<typeof UserKey>
+
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
@@ -22,7 +32,7 @@ export default class User extends BaseModel {
   public updatedAt: DateTime
 
   @beforeSave()
-  public static async hashPassword (user: User) {
+  public static async hashPassword(user: User) {
     if (user.$dirty.password) {
       user.password = await Hash.make(user.password)
     }
