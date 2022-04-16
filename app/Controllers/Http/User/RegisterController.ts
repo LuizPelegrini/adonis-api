7e351e25ded8e184fe17d21a1d727f40b458838f
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2022-04-16 18:35:06 +0800

    add: send email to user in order to finish the account creation

--- app/Controllers/Http/User/RegisterController.ts
+++ app/Controllers/Http/User/RegisterController.ts
@@ -2,11 +2,14 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { StoreValidator } from 'App/Validators/User/Register'
 import { User } from 'App/Models'
 import { faker } from '@faker-js/faker'
+import Mail from '@ioc:Adonis/Addons/Mail'
 
 export default class RegisterController {
   public async store({ request, response }: HttpContextContract) {
+    // validate request body
     const { email, redirectUrl } = await request.validate(StoreValidator)
 
+    // create user and save it
     const user = await User.create({ email })
     await user.save()
 
@@ -18,7 +21,13 @@ export default class RegisterController {
 
     const link = `${redirectUrl.replace(/\/$/, '')}/${key}`
 
-    //TODO: send email
+    // send email
+    await Mail.send((message) => {
+      message.to(email)
+      message.from('contact@facebook.com', 'Facebook')
+      message.subject('Account Creation')
+      message.htmlView('emails/register', { link })
+    })
 
     return response.noContent()
   }
--- start/routes/index.ts
+++ start/routes/index.ts
@@ -1,10 +1,7 @@
 import Route from '@ioc:Adonis/Core/Route'
 import './auth'
+import './user'
 
 Route.get('/', async () => {
   return { hello: 'world' }
-})
-
-Route.get('/user-register', async ({ view }) => {
-  return view.render('emails/register')
 })
