98006901b4e71dfa6f4233df94c619c52b4e01c2
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2022-05-07 17:03:10 +0800

    update: user should be able to follow/unfollow other user

---
+++ app/Controllers/Http/User/FollowsController.ts
@@ -0,0 +1,36 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { StoreValidator, DestroyValidator } from 'App/Validators/User/Follows'
+
+export default class FollowsController {
+  public async store(ctx: HttpContextContract) {
+    const { request, params, auth, response } = ctx
+
+    // validate path parameter
+    const { followingId } = await request.validate({
+      data: {
+        followingId: params.id,
+      },
+      schema: new StoreValidator(ctx).schema,
+    })
+
+    if (auth.user!.id === followingId) {
+      return response.unprocessableEntity({ error: { message: `User can't follow himself ` } })
+    }
+
+    await auth.user!.related('following').attach([followingId])
+  }
+
+  public async destroy(ctx: HttpContextContract) {
+    const { request, params, auth, response } = ctx
+
+    // validate path parameter
+    const { followingId } = await request.validate({
+      data: {
+        followingId: params.id,
+      },
+      schema: new DestroyValidator(ctx).schema,
+    })
+
+    await auth.user!.related('following').detach([followingId])
+  }
+}
--- start/routes/user.ts
+++ start/routes/user.ts
@@ -1,5 +1,8 @@
 import Route from '@ioc:Adonis/Core/Route'
 
+Route.get('/users', 'User/MainController.show').middleware('auth')
+Route.put('/users', 'User/MainController.update').middleware('auth')
+
 Route.post('/users/register', 'User/RegisterController.store')
 Route.get('/users/register/:key', 'User/RegisterController.show')
 Route.put('/users/register', 'User/RegisterController.update')
@@ -7,10 +10,10 @@ Route.put('/users/register', 'User/RegisterController.update')
 Route.post('/users/password-recovery', 'User/PasswordRecoveryController.store')
 Route.put('/users/password-recovery', 'User/PasswordRecoveryController.update')
 
-Route.get('/users', 'User/MainController.show').middleware('auth')
-Route.put('/users', 'User/MainController.update').middleware('auth')
-
 Route.get('/users/search', 'User/SearchController.index').middleware('auth')
 
 Route.put('/users/avatar', 'User/AvatarController.update').middleware('auth')
 Route.delete('/users/avatar', 'User/AvatarController.destroy').middleware('auth')
+
+Route.post('/users/follow/:id', 'User/FollowsController.store').middleware('auth')
+Route.delete('/users/follow/:id', 'User/FollowsController.destroy').middleware('auth')
